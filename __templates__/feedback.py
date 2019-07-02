a6df7cc83e00a4595322f90ff1bea37e43215acc
jy95 <jacques.yakoub@gmail.com>
2019-07-02 14:34:58 +0200

    Set up structure

--- __templates__/feedback.py
+++ __templates__/feedback.py
@@ -1,2 +1,54 @@
+import sys
+import os
 from inginious import rst, feedback
 
+
+# Throw a fatal error if the given code doesn't compile
+def compilation_feedback(problem_id, result):
+    if result.stderr:
+        msg = "Votre fichier n'a pas compilé ou une erreur s'est produite : {}".format(result.stderr)
+        feedback.set_problem_feedback(msg, problem_id)
+        feedback.set_global_result("Erreur de compilation (ou autre)")
+        sys.exit(0)
+
+
+# Append a failure/error log in the result file
+def append_log(name, kind="Exception", method="", result_file_path="student/task.result"):
+    # if file doesn't exist yet, creates it
+    with open(result_file_path, "w+" if os.stat(result_file_path).st_size == 0 else "a+") as file:
+        file.write("{}:{}:{}".format(
+                "E" if kind == "Exception" else "F",
+                name,
+                method
+            )
+        )
+
+
+def generate_feedback(result_file_path="student/task.result"):
+    with open(result_file_path, 'r', encoding='utf-8') as file:
+        # empty file : no problem
+        if os.stat(result_file_path).st_size == 0:
+            msg = "Votre code a passé avec succès tous les tests pour cette mission."
+            feedback.set_global_result(msg)
+            sys.exit(0)
+        # not empty file ; each problem is encoded on one line with the following format
+        # Kind:Class:....
+        else:
+            for line in file:
+                line = line.strip()
+                # by default, there is at least the name of ; second one is optional
+                elems = line.split(':')
+                name = elems[0]
+                method = elems[1]
+
+                # Two kind of problems : exception (E) and failure (F)
+                # Messages follows the same structure
+                msg = "{}  : la classe {} {} {} ) \n" \
+                    .format(
+                        "Exception" if line[0] == "E" else "Erreur",
+                        name,
+                        "a lancé une exception" if line[0] == "E" else "n'a pas passé les tests",
+                        "( méthode {} )".format(method) if method != "" else ""
+                    )
+                # Appends global message
+                feedback.set_global_result(msg, True)
--- __templates__/helper.py
+++ __templates__/helper.py
@@ -1,12 +1,14 @@
 #!/bin/python3
 
 # les imports
-import subprocess, shlex
+import subprocess
+import shlex
 from inginious import input
 from collections import namedtuple
 from pathlib import Path
 
 ProcessOutput = namedtuple('ProcessOutput', ['returncode', 'stdout', 'stderr'])
+
 
 # Libraries to be add to run/compile via option -cp of java(c)
 def librairies():
@@ -15,15 +17,17 @@ def librairies():
     # lib += ':/usr/share/java/powermock-mockito2-junit-1.7.1/*'
     return lib
 
+
 # Wrapper to execute system commands and easily get stdout, stderr steams and return code
 def runCommand(cmd):
     proc = subprocess.Popen(shlex.split(cmd), stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    proc.wait() #waiting the child process to finish
+    proc.wait()  # waiting the child process to finish
 
     stdout = proc.stdout.read().decode('utf-8')
-    stderr = proc.stderr.read()#.decode('utf-8')
+    stderr = proc.stderr.read()  # .decode('utf-8')
 
     return ProcessOutput(proc.returncode, stdout, stderr)
+
 
 # Store the given problemid code into the base_path folder ( "student" by default) and return the filename
 def store_uploaded_file(problem_id, base_path="student"):
@@ -38,6 +42,7 @@ def store_uploaded_file(problem_id, base_path="student"):
 
     return filename_with_path
 
+
 # Apply parse_template on each file stored in base_path
 def apply_templates(base_path="student"):
     basepath = Path("{}/".format(base_path))
@@ -49,19 +54,21 @@ def apply_templates(base_path="student"):
     for file in only_top_level_files:
         input.parse_template(file)
 
+
 # Generate compile/run command for given file
 # Bonus : it removes the ".java" extension for javac
 def generate_command_string(filename, command="java", classpath="student"):
     libs = librairies()
     command_code = "{} -classpath {} {} {}" \
         .format(
-        command,
-        classpath,
-        # Something, no need of libraries so include only if needed
-        '' if libs == '' else '-cp {}'.format(libs),
-        filename if command == "javac" else basename_filename(filename)
-    )
+            command,
+            classpath,
+            # Something, no need of libraries so include only if needed
+            '' if libs == '' else '-cp {}'.format(libs),
+            filename if command == "javac" else basename_filename(filename)
+        )
     return command_code
+
 
 # Extract the filename basename without extension
 # And returns only the name
