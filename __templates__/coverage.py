8550925647055ac99ed405f9bf32e3b623caf5a1
jy95 <jacques.yakoub@gmail.com>
2019-07-04 16:39:36 +0200

    Basic set up for coverage

---
+++ __templates__/coverage.py
@@ -0,0 +1,37 @@
+from xml.etree import ElementTree as ET
+
+
+# https://docs.python.org/3/library/xml.etree.elementtree.html
+# Extract the stats given by Jacoco into a list so that we can use that later
+def extract_stats(path_to_xml_file):
+    tree = ET.parse(path_to_xml_file)
+    root = tree.getroot()
+    return [
+        {
+            "covered": int(coverage_data.get("covered")),
+            "missed": int(coverage_data.get("missed")),
+            "type": coverage_data.get("type")
+        }
+        for coverage_data in root.findall("/report/counter")
+    ]
+
+
+# TODO A Tester
+# Command to generate a exec file from JaCoCo
+def exec_file_generation_command(file):
+    return "{} –javaagent:{} {}".format(
+        "java",
+        "/course/common/jacocoagent.jar",
+        file[:-5]
+    )
+
+
+# TODO A Tester
+# Command to generate the result as a xml file from JaCoCo
+def generate_coverage_report(exec_file, xml_output):
+    return "{} -jar {} report {} --xml {}".format(
+        "java",
+        "/course/common/jacococli.jar",
+        exec_file,
+        xml_output
+    )
