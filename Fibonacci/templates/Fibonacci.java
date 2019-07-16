6e9e53d5e4db958588b966a2b0d52df6386725f1
alexandre <alex.rucquoy@outlook.com>
2019-07-16 10:00:48 +0200

    Fibonacci, testable

---
+++ Fibonacci/templates/Fibonacci.java
@@ -0,0 +1,30 @@
+package src;
+
+
+public class Fibonacci {
+
+
+    private static int count = 0;
+
+    public static int getCount(){
+        return count;
+    }
+
+    public static void resetCount(){
+        count = 0;
+    }
+
+
+    public static int fiboRecursive(int index){
+
+        count++;
+
+    @   @student_recursive@@
+    }
+
+
+    public static int fiboIterative(int index){
+    @   @student_iterative@@
+    }
+
+}
