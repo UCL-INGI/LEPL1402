d4fc4d7092debdd4a6d4c668e86204fbd7e86b07
alexandre <alex.rucquoy@outlook.com>
2019-07-17 11:27:53 +0200

    Refactored visitor, had a better idea

--- VisitorBasic/public/Visitor.java
+++ VisitorBasic/public/Visitor.java
@@ -1,18 +1,27 @@
+import java.util.ArrayList;
 import java.util.List;
 
 abstract class Visitor {
 
-    protected List<? extends Object> filtered;
+    protected Class toFilter;
+    protected List<Object> filtered;
+
+    public Visitor(Class cls){
+        this.filtered = new ArrayList<>();
+        this.toFilter = cls;
+    }
 
     /*
      * returns "filtered" list. Should only be called
      * after the visitor traversed the whole list.
      */
-    abstract List<? extends Object> getFiltered();
+    abstract List<Object> getFiltered();
+
+    abstract void visit(Visitable visitable);
 
     /*
-     * get the Visitable elements and filter out any element
-     * that is NOT an Integer 
+     * If o is an instance of 'toFilter', add it to filtered.
      */
-    abstract void visit(Visitable visitable);
+    abstract void visit(Object o);
+}
 }
--- VisitorBasic/src/InginiousTests.java
+++ VisitorBasic/src/InginiousTests.java
@@ -13,7 +13,7 @@ import static org.junit.Assert.*;
 @RunWith(GradingRunner.class)
 public class InginiousTests {
 
-    private Class [] cls = {Integer.class, Float.class, Double.class, String.class, Long.class};
+    private Class [] cls = {Integer.class, Double.class, String.class, Long.class};
 
     private Supplier [] suppliers = new Supplier[]{
             () ->  (int) (Math.random()*100) +1, // int
@@ -29,32 +29,37 @@ public class InginiousTests {
     @Grade
     public void Randomtest(){
 
-        for(int n = 0; n < 10; n++) { // repeat 10 times the test, in case of luck.
+        for(Class c : cls) {
 
-            Object[] random = new Object[30];
+            for (int n = 0; n < 10; n++) { // repeat 10 times the test, in case of luck.
 
-            for (int i = 0; i < random.length; i++) {
-                random[i] = suppliers[rng.get()].get();
-            }
 
-            Visitable list = new VisitableList(random);
-            Visitor visitor = new VisitorList();
+                Object[] random = new Object[30];
 
-            list.accept(visitor);
-
-            List<? extends Object> result = visitor.getFiltered();
-
-            for (int i = 0; i < random.length; i++) {
-
-                if (random[i] instanceof Integer) {
-                    assertTrue(result.contains(random[i]));
+                for (int i = 0; i < random.length; i++) {
+                    random[i] = suppliers[rng.get()].get();
                 }
-            }
 
-            for (int i = 0; i < result.size(); i++) {
-                assertTrue(result.get(i) instanceof Integer);
-            }
+                Visitable list = new VisitableList(random);
+                Visitor visitor = new VisitorList(c);
 
+                visitor.visit(list);
+
+                List<Object> result = visitor.getFiltered();
+
+                for (int i = 0; i < random.length; i++) {
+
+                    if (random[i].getClass().equals(c)) {
+                        assertTrue(result.contains(random[i]));
+                    }
+                }
+
+                for (int i = 0; i < result.size(); i++) {
+                    assertEquals(c, result.get(i).getClass());
+                }
+
+            }
         }
     }
+
 }
