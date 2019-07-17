df4d119fcff93a82abb93b00724ffac923664513
alexandre <alex.rucquoy@outlook.com>
2019-07-17 08:54:10 +0200

    Updated Blackbox testing for inginious (vodoo timeouts idk)

--- BlackBox/templates/StudentTests.java
+++ BlackBox/templates/StudentTests.java
@@ -8,12 +8,15 @@ import src.JailRunner;
 import src.Exercise;
 import static org.junit.Assert.*;
 
+// CAREFUL WHEN YOU SET A defaultCpuTimeout WITH INGINIOUS, ALWAYS SET IT LOWER THAN WHAT
+// THE VALUE YOU CHOSE FOR YOUR OWN MACHINE
+
 @RunWith(JailRunner.class)
-@GradeClass(totalValue = 1, defaultCpuTimeout = 2000)
+@GradeClass(totalValue = 1, defaultCpuTimeout = 500)
 public class StudentTests {
 
     // STUDENT CODE WILL BE INSERTED HERE
-@ @student_code@@
+@   @student_code@@
     // END OF STUDENT CODE
 
 }
