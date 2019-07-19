02237bc441f123af3bd09bd4c4209295f7de8a21
alexandre <alex.rucquoy@outlook.com>
2019-07-19 16:45:47 +0200

    Binary tree equals, testable on inginious

---
+++ TreeSame/templates/Node.java
@@ -0,0 +1,22 @@
+package src;
+
+public class Node {
+
+    public int val;
+    public Node left;
+    public Node right;
+
+    public Node(int val){
+        this.val = val;
+    }
+
+    public boolean isLeaf(){
+        return this.left == null && this.right == null;
+    }
+
+    @Override
+    public boolean equals(Object o){
+    @   @student_eqnode@@
+    }
+
+}
---
+++ TreeSame/templates/Tree.java
@@ -0,0 +1,16 @@
+package src;
+
+public class Tree {
+
+    public Node root;
+
+    public Tree(Node root){
+        this.root = root;
+    }
+
+
+    @Override
+    public boolean equals(Object o){
+    @   @student_eqtree@@
+    }
+}
