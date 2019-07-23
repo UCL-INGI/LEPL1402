c6a50727e3ccffe0814a1563bdae825d2797cd21
alexandre <alex.rucquoy@outlook.com>
2019-07-23 11:01:19 +0200

    Inorder tree traversal, testable on inginious

---
+++ TreeInorder/templates/Traversal.java
@@ -0,0 +1,27 @@
+package src;
+
+import java.util.List;
+import java.util.Stack; // this should give you a hint for the iterative version
+
+public class Traversal {
+
+    public static int nCalls = 0;
+
+    public static void recursiveInorder(Node root, List<Integer> res) {
+
+        nCalls++;
+
+    @   @student_recursive@@
+
+    }
+
+
+    public static void iterativeInorder(Node root, List<Integer> res){
+
+        nCalls++;
+
+    @   @student_iterative@@
+
+    }
+
+}
