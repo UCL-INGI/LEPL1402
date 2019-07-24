f0c7ab0a63b5b4b052d9594b3c111cdc94f6c3fb
alexandre <alex.rucquoy@outlook.com>
2019-07-24 15:40:37 +0200

    New task : string manipulation in java. It's goddamn hot outside, I can't stand it..

---
+++ StringUtils/public/StringUtils.java
@@ -0,0 +1,38 @@
+public class StringUtils {
+
+
+    /*
+     * Split the input string 'str' w.r.t the character 'marker' in an array of String
+     * for example split("test-test", '-') => {"test", "test"}
+     * Must return null if there is no occurrence of 'marker' in 'str'
+     */
+    public static String [] split(String str, char marker){
+
+    }
+
+
+    /*
+     * Returns the index of the first occurrence of sub in str
+     * or -1 if there is no occurrence of sub in str at all.
+     * Be careful, we ask you to make CASE SENSITIVE comparison between str and sub.
+     */
+    public static int indexOf(String str, String sub){
+
+    }
+
+
+    public static String toLowerCase(String str){
+
+    }
+
+
+    /*
+     * Returns true if the string 'str' is a palindrome (a string that reads the same from
+     * left to right AND from right to left).
+     */
+    public static boolean palindrome(String str){
+    
+    }
+
+
+}
---
+++ StringUtils/templates/StringUtils.java
@@ -0,0 +1,43 @@
+package src;
+
+
+public class StringUtils {
+
+
+    /*
+     * Split the input string 'str' w.r.t the character 'marker' in an array of String
+     * for example split("test-test", '-') => {"test", "test"}
+     * Must return null if there is no occurrence of 'marker' in 'str'
+     */
+    public static String [] split(String str, char marker){
+
+    @   @student_split@@
+
+    }
+
+
+    /*
+     * Returns the index of the first occurrence of sub in str
+     * or -1 if there is no occurrence of sub in str at all.
+     * Be careful, we ask you to make CASE SENSITIVE comparison between str and sub.
+     */
+    public static int indexOf(String str, String sub){
+    @   @student_indexof@@
+    }
+
+
+    public static String toLowerCase(String str){
+    @   @student_tolowercase@@
+    }
+
+
+    /*
+     * Returns true if the string 'str' is a palindrome (a string that reads the same from
+     * left to right AND from right to left).
+     */
+    public static boolean palindrome(String str){
+    @   @student_palindrome@@
+    }
+
+
+}
