7f87ddd1eb4b8b58bd9095197694b77deb85141c
jy95 <jacques.yakoub@gmail.com>
2019-07-26 13:30:07 +0200

    Working on testing of TreeCombineWith

--- TreeCombineWith/src/InginiousTests.java
+++ TreeCombineWith/src/InginiousTests.java
@@ -10,9 +10,11 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import templates.*;
@@ -21,14 +23,67 @@ import templates.*;
 @RunWith(GradingRunner.class)
 public class InginiousTests {
 
-    private Supplier<Integer> rng = () -> (int) (Math.random() * 25) + 5; // at least 5 elements
+    // for testing
+    private static int MINIMAL_AMOUNT_OF_ELEMENTS_IN_TREE = 1;
+    private static int MAXIMAL_AMOUNT_OF_ELEMENTS_IN_TREE = 17;
+    private static int MINIMAL_VALUE = -15;
+    private static int MAXIMAL_VALUE = 15;
+
+    // to generate a random tree of n element(s)
+    private int[] generateIntArray(){
+        int size = new Random()
+                .ints(1,
+                        InginiousTests.MINIMAL_AMOUNT_OF_ELEMENTS_IN_TREE,
+                        InginiousTests.MAXIMAL_AMOUNT_OF_ELEMENTS_IN_TREE
+                )
+                .sum();
+        return new Random().ints(size, InginiousTests.MINIMAL_VALUE, InginiousTests.MAXIMAL_VALUE).toArray();
+    }
+
+    // to generate a tree given a int array
+    private Tree geneateRandomTree(int[] array) {
+        Node root = null;
+        root = insertLevelOrder(array, root, 0);
+        return new Tree(root);
+    }
+
+    // inspired by https://www.geeksforgeeks.org/construct-complete-binary-tree-given-array/
+    private Node insertLevelOrder(int[] arr, Node root, int i) {
+        // Base case for recursion
+        if (i < arr.length) {
+            root = new Node(arr[i]);
+
+            // insert left child
+            root.left = insertLevelOrder(arr, root.left,
+                    2 * i + 1);
+
+            // insert right child
+            root.right = insertLevelOrder(arr, root.right,
+                    2 * i + 2);
+        }
+        return root;
+    }
+
+    // Credits to https://www.quora.com/How-do-I-add-two-arrays-in-Java-and-initialize-the-third-array-with-the-sum-of-the-two-corresponding-elements-from-the-two-arrays/answer/Mar-Souf
+    private int[] sum (int[] a, int[] b) {
+        return IntStream
+                .range(0, Math.max(a.length, b.length))
+                .map(index -> (index < a.length ? a[index] : 0) + (index < b.length ? b[index] : 0))
+                .toArray();
+    }
 
     @Test
     @Grade
     @GradeFeedbacks({@GradeFeedback(onSuccess = true, message = ""),
             @GradeFeedback(onFail = true, message = "")})
     public void test1() {
+        int[] data1 = generateIntArray();
+        int[] data2 = generateIntArray();
+        int[] result = sum(data1, data2);
+        Tree t1 = geneateRandomTree(data1);
+        Tree t2 = geneateRandomTree(data2);
 
+        System.out.println();
     }
 
 }
