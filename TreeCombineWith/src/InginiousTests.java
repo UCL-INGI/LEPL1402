7e34bd7795e4f2fa01aefba2eebce8d4c6e3354d
jy95 <jacques.yakoub@gmail.com>
2019-07-26 14:32:59 +0200

    Working on combineWIth exercise

--- TreeCombineWith/src/InginiousTests.java
+++ TreeCombineWith/src/InginiousTests.java
@@ -10,12 +10,8 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import org.junit.runner.RunWith;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
-import java.util.function.Supplier;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import templates.*;
 
@@ -30,7 +26,7 @@ public class InginiousTests {
     private static int MAXIMAL_VALUE = 15;
 
     // to generate a random tree of n element(s)
-    private int[] generateIntArray(){
+    private int[] generateIntArray() {
         int size = new Random()
                 .ints(1,
                         InginiousTests.MINIMAL_AMOUNT_OF_ELEMENTS_IN_TREE,
@@ -65,7 +61,7 @@ public class InginiousTests {
     }
 
     // Credits to https://www.quora.com/How-do-I-add-two-arrays-in-Java-and-initialize-the-third-array-with-the-sum-of-the-two-corresponding-elements-from-the-two-arrays/answer/Mar-Souf
-    private int[] sum (int[] a, int[] b) {
+    private int[] sum(int[] a, int[] b) {
         return IntStream
                 .range(0, Math.max(a.length, b.length))
                 .map(index -> (index < a.length ? a[index] : 0) + (index < b.length ? b[index] : 0))
@@ -75,15 +71,60 @@ public class InginiousTests {
     @Test
     @Grade
     @GradeFeedbacks({@GradeFeedback(onSuccess = true, message = ""),
-            @GradeFeedback(onFail = true, message = "")})
+            @GradeFeedback(onFail = true, message = "Your implementation doesn't hold against the given example")})
     public void test1() {
-        int[] data1 = generateIntArray();
-        int[] data2 = generateIntArray();
-        int[] result = sum(data1, data2);
-        Tree t1 = geneateRandomTree(data1);
-        Tree t2 = geneateRandomTree(data2);
+        Tree t1 = new Tree(
+                new Node(
+                        9,
+                        new Node(6,
+                                new Node(9),
+                                new Node(2, new Node(4), null)
+                        ),
+                        new Node(14,
+                                null,
+                                new Node(11)
+                        )
+                )
+        );
 
-        System.out.println();
+        Tree t2 = new Tree(
+                new Node(
+                        0,
+                        new Node(-3, new Node(8), null),
+                        new Node(8, new Node(5, null, new Node(1)), new Node(6))
+                )
+        );
+
+        Tree expected = new Tree(
+                new Node(
+                        9,
+                        new Node(3,
+                                new Node(17),
+                                new Node(2, new Node(4), null)
+                        ),
+                        new Node(22,
+                                new Node(5, null, new Node(1)),
+                                new Node(17)
+                        )
+                )
+        );
+        assertEquals(expected.root, t1.combineWith(t2).root);
+    }
+
+    @Test
+    @Grade
+    @GradeFeedbacks({@GradeFeedback(onSuccess = true, message = ""),
+            @GradeFeedback(onFail = true, message = "Your implementation doesn't hold against random input")})
+    public void test2() {
+        for (int i = 0; i < 100; i++) {
+            int[] data1 = generateIntArray();
+            int[] data2 = generateIntArray();
+            int[] result = sum(data1, data2);
+            Tree t1 = geneateRandomTree(data1);
+            Tree t2 = geneateRandomTree(data2);
+            Tree expected = geneateRandomTree(result);
+            assertEquals(expected.root, t1.combineWith(t2).root);
+        }
     }
 
 }
