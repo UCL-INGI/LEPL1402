781abd512e81b2b132b1a6c9e2ff35db84d7cc37
jy95 <jacques.yakoub@gmail.com>
2019-08-12 21:57:33 +0200

    Testing : set up a basic "Coverage Testing"

--- travis_setup.py
+++ travis_setup.py
@@ -7,12 +7,16 @@ os.mkdir(folder_dir)
 
 items = [item.name
          for item in os.scandir('.')
-         if item.is_dir() and item.name != folder_dir]
+         if item.is_dir() and item.name not in {folder_dir, "$test"}]
 
 # Copy tasks to create folder
 for item in items:
     shutil.copytree('./' + item, "{}/".format(folder_dir) + item)
-    os.mkdir(os.path.join("{}/{}/".format(folder_dir, item),'test'))
+    os.mkdir(os.path.join("{}/{}/".format(folder_dir, item), 'test'))
 
 # Copy course.yaml
 shutil.copy2('./course.yaml', "{}/course.yaml".format(folder_dir))
+
+# Prepare the $test$ folder for unit test of feedback
+shutil.copytree('./$common', "$test$/$common")
+shutil.copy2('./course.yaml', "$test$/course.yaml")
