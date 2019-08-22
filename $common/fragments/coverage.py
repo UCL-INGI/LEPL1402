8ee3ba6bdfee2373ab6057711c3dff15cc4b4223
jy95 <jacques.yakoub@gmail.com>
2019-08-23 01:20:06 +0200

    fix(merge): Fix mess introduced by git push (#10)

--- $common/fragments/coverage.py
+++ $common/fragments/coverage.py
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-import shutil
 from xml.etree import ElementTree as ET
 from fragments.constants import *
 
@@ -31,36 +29,4 @@ def generate_coverage_report(exec_file=JACOCO_EXEC_FILE,
         ' '.join(["--classfiles {}".format(str(c)) for c in classes_path]),
         xml_output
     )
-=======
-from xml.etree import ElementTree as ET
-from fragments.constants import *
 
-
-# https://docs.python.org/3/library/xml.etree.elementtree.html
-# Extract the stats given by Jacoco into a list so that we can use that later
-def extract_stats(path_to_xml_file=JACOCO_RESULT_FILE):
-    tree = ET.parse(path_to_xml_file)
-    root = tree.getroot()
-    return [
-        {
-            "covered": int(coverage_data.get("covered")),
-            "missed": int(coverage_data.get("missed")),
-            "type": coverage_data.get("type")
-        }
-        for coverage_data in root.findall("./counter")
-    ]
-
-
-# Command to generate the result as a xml file from JaCoCo
-# https://stackoverflow.com/questions/47717538/usage-of-jacococli-jar-with-multiple-classfiles
-def generate_coverage_report(exec_file=JACOCO_EXEC_FILE,
-                             classes_path=JACOCO_CLASS_FILES,
-                             xml_output=JACOCO_RESULT_FILE):
-    return "{} -jar {} report {} {} --xml {}".format(
-        "java",
-        str(Path(LIBS_FOLDER) / "jacococli.jar"),
-        exec_file,
-        ' '.join(["--classfiles {}".format(str(c)) for c in classes_path]),
-        xml_output
-    )
->>>>>>> 1f7760a0446ed66fd5a4f61855d8bb2b30173325
