7c6a23e9ce37393b86742e9991f929f60a1635be
jy95 <jacques.yakoub@gmail.com>
2019-11-08 20:42:40 +0100

    Refactoring of the testing tool

--- check_requirements.py
+++ check_requirements.py
@@ -10,6 +10,65 @@ def text_with_color(text, color):
     return "%s%s %s %s" % (fg(color), bg(0), text, attr(0))
 
 
+# The used validations for feedback_settings.yml
+def validations(feedback_settings):
+    def verification_result(validation_kind, validation_fct):
+        return validation_fct() if validation_kind in feedback_settings else ""
+
+    def feedback_kind_check():
+        return "{}{}".format(
+            text_with_color("You must set has_feedback to True in order to have feedback on INGInious", 196) + "\n"
+            if ("has_feedback" not in feedback_settings) or not feedback_settings["has_feedback"] else "",
+            text_with_color("You must have a flavour folder for coverage testing", 196) + "\n"
+            if feedback_settings["feedback_kind"] == "JaCoCo" and "flavour" not in files_and_folder_in_task else ""
+        )
+
+    def quorum_check():
+        if not isinstance(feedback_settings["quorum"], float):
+            return text_with_color("Quorum should be a float", 196) + "\n"
+        else:
+            if feedback_settings["quorum"] > 1.0 or feedback_settings["quorum"] < 0.0:
+                return text_with_color("Quorum should be a float between 0.0 and 1.0", 196) + "\n"
+            else:
+                return ""
+
+    def coverage_stats_check():
+        if not isinstance(feedback_settings["coverage_stats"], list):
+            return text_with_color("coverage_stats should be a sequence of string", 196) + "\n"
+        else:
+            if any(
+                    [item not in ["INSTRUCTION", "BRANCH", "LINE", "METHOD", "CLASS"]
+                     for item in feedback_settings["coverage_stats"]]
+            ):
+                return text_with_color("Unknown value inside coverage_stats", 196) + "\n"
+            else:
+                return ""
+
+    def statement_presence_check(statement_kind):
+        def checker():
+            if not isinstance(feedback_settings[statement_kind], dict):
+                return text_with_color("{} should be a dictionary (String, List/Sequence) "
+                                       .format(statement_kind), 196) + "\n"
+            else:
+                if any([
+                    (not isinstance(problem_id, str)) or (not isinstance(statements, list))
+                    for (problem_id, statements) in feedback_settings[statement_kind].items()
+                ]):
+                    return text_with_color("{} is improperly constructed".format(statement_kind), 196) + "\n"
+                else:
+                    return ""
+
+        return checker
+
+    return iter([
+        verification_result("feedback_kind", feedback_kind_check),
+        verification_result("quorum", quorum_check),
+        verification_result("coverage_stats", coverage_stats_check),
+        verification_result("prohibited", statement_presence_check("prohibited")),
+        verification_result("required", statement_presence_check("required")),
+    ])
+
+
 print("CHECK THAT ALL TASKS FOLLOWS THE EXPLAINED STRUCTURE IN DOC")
 
 # As inginious-autotest required a folder where everything is stored, use that
@@ -41,57 +100,8 @@ for folder in task_folders:
 
         with open(feedback_path, "r") as stream:
             try:
-                result = yaml.safe_load(stream)
-
-                # check if flavour is needed
-                if "feedback_kind" in result:
-
-                    if ("has_feedback" not in result) or not result["has_feedback"]:
-                        problem += text_with_color(
-                            "You must set has_feedback to True in order to have feedback on INGInious", 196) + "\n"
-
-                    if result["feedback_kind"] == "JaCoCo" and "flavour" not in files_and_folder_in_task:
-                        problem += text_with_color("You must have a flavour folder for coverage testing", 196) + "\n"
-
-                if "quorum" in result:
-
-                    if not isinstance(result["quorum"], float):
-                        problem += text_with_color("Quorum should be a float", 196) + "\n"
-
-                    else:
-
-                        if result["quorum"] > 1.0 or result["quorum"] < 0.0:
-                            problem += text_with_color("Quorum should be a float between 0.0 and 1.0", 196) + "\n"
-
-                if "coverage_stats" in result:
-
-                    if not isinstance(result["coverage_stats"], list):
-                        problem += text_with_color("coverage_stats should be a sequence of string", 196) + "\n"
-
-                    else:
-
-                        if any([
-                            item not in ["INSTRUCTION", "BRANCH", "LINE", "METHOD", "CLASS"]
-                            for item in result["coverage_stats"]]
-                        ):
-                            problem += text_with_color("Unknown value inside coverage_stats", 196) + "\n"
-
-                for statement_check in ["prohibited", "required"]:
-
-                    if statement_check in result:
-
-                        if not isinstance(result[statement_check], dict):
-                            problem += text_with_color(
-                                "{} should be a dictionary (String, List/Sequence) ".format(statement_check), 196) \
-                                       + "\n"
-                        else:
-                            if any([
-                                (not isinstance(problem_id, str)) or (not isinstance(statments, list))
-                                for (problem_id, statments) in result[statement_check].items()
-                            ]):
-                                problem += text_with_color(
-                                    "{} is improperly constructed".format(statement_check), 196) + "\n"
-
+                feedback_settings = yaml.safe_load(stream)
+                problem += "".join(validations(feedback_settings))
             except yaml.YAMLError as exc:
                 problem += text_with_color("Parsing error in feedback_settings.yaml\n", 196)
 
