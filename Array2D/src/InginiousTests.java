2f8e273102b7fa15daecc1e626f886027be7283b
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2020-09-15 17:16:09 +0200

    modify Array2D exercise

--- Array2D/src/InginiousTests.java
+++ Array2D/src/InginiousTests.java
@@ -1,186 +1,118 @@
 package src;
-
-import com.github.guillaumederval.javagrading.Grade;
-import com.github.guillaumederval.javagrading.GradeFeedback;
-import com.github.guillaumederval.javagrading.GradeFeedbacks;
-import com.github.guillaumederval.javagrading.GradingRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import java.util.function.Supplier;
-import java.util.stream.Stream;
+import com.github.guillaumederval.javagrading.GradingRunner;
+import com.github.guillaumederval.javagrading.Grade;
 
-import java.util.*;
+import java.util.stream.Collectors;
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.Random;
 import templates.*;
 
-import static org.junit.Assert.*;
-
-@RunWith(GradingRunner.class) // classic "jail runner" from Guillaume's library
+@RunWith(GradingRunner.class)
 public class InginiousTests {
 
-    // generate random number
-    private Supplier<Integer> rng = () -> (int) ((Math.random()*100) + 1); // never generate 0
-    // random number of row line (just in order to limit the matrix product or other)
-    private Supplier<Integer> rng2 = () -> (int) ((Math.random()*10) + 1); // never generate 0
-    // Student code
-    private Array2DBuilderInterface student_code = new MyBuilder();
+    final private Random rand = new Random();
 
-    // build a regular matrix
-    private int[][] generate_regular_matrix(int number_of_row, int number_of_column) {
-        int [][] original = new int[number_of_row][];
-
-        for(int row = 0; row < number_of_row; row++) {
-            int [] seeds = Stream.generate(rng).limit(number_of_column).mapToInt(i -> i).toArray();
-            original[row] = seeds;
-        }
-
-        return original;
-    }
-
-    // compute matrix multiplication
-    // Credits to https://www.programiz.com/java-programming/examples/multiply-matrix-function
-    // (code on stackoveflow was ugly as hell)
-    private int[][] multiplyMatrices(int[][] firstMatrix, int[][] secondMatrix, int r1, int c1, int c2) {
-        int[][] product = new int[r1][c2];
-        for(int i = 0; i < r1; i++) {
-            for (int j = 0; j < c2; j++) {
-                for (int k = 0; k < c1; k++) {
-                    product[i][j] += firstMatrix[i][k] * secondMatrix[k][j];
-                }
+    private int[][] generateRectangularMatrix() {
+        int numberOfRow = rand.nextInt(20) + 1;
+        int numberOfColumn = rand.nextInt(20) + 1;
+        int [][] matrix = new int[numberOfRow][numberOfColumn];
+        for (int row = 0; row < numberOfRow; row++) {
+            for (int col = 0; col < numberOfColumn; col++) {
+                matrix[row][col] = rand.nextInt();
             }
         }
-        return product;
+        return matrix;
     }
 
-    // Test for build_from
+    private int[][] generateNonRectangularMatrix() {
+        int numberOfRow = rand.nextInt(20) + 1;
+        int [][] matrix = new int[numberOfRow][];
+        for (int row = 0; row < numberOfRow; row++) {
+            int numberOfCol = rand.nextInt(20) + 1;
+            matrix[row] = new int[numberOfCol];
+            for (int col = 0; col < numberOfCol; col ++) {
+                matrix[row][col] = rand.nextInt();
+            }
+        }
+        return matrix;
+    }
+
+    private String matrixtoStr(int [][] matrix) {
+        return Arrays.stream(matrix)
+            .map(row -> Arrays.stream(row).mapToObj(String::valueOf).collect(Collectors.joining(" ")))
+            .collect(Collectors.joining("\n"));
+    }
+
     @Test
-    @Grade(value=1, cpuTimeout=100)
-    @GradeFeedbacks({@GradeFeedback(message = "", onSuccess = true),
-    @GradeFeedback(message = "build_from(String s) does not work\n", onFail = true, onTimeout = true)})
+    @Grade(value=1)
     public void testBuildFrom() {
-        
         for(int i = 0; i < 10; i++) {
-            
-            // generate an 2D array that will mostly be an irregular matrix
-            int number_of_row = rng2.get();
-            int [][] expected = new int[number_of_row][];
-            StringBuilder str = new StringBuilder(); 
-
-            for(int row = 0; row < number_of_row; row++) {
-                
-                int [] seeds = Stream.generate(rng).limit(rng2.get()).mapToInt(j -> j).toArray();
-                for(int v: seeds) {
-                    str.append(String.valueOf(v) + " ");
-                }
-                expected[row] = seeds;
-                str.append("\n");
-                if (row != number_of_row -1) {
-                    str.append(" ");
-                }
-
-            }
-
-            // Time to stringify this thing for student
-            String input = str.toString();
-            assertTrue(
-                Arrays.deepEquals(
-                    expected, 
-                    student_code.build_from(input)
-                )
-            );
-
+            int [][] expected = generateNonRectangularMatrix();
+            String input = matrixtoStr(expected);
+            int [][] result = Matrix.buildFrom(input);
+            assertArrayEquals(expected, result);
         }
     }
 
-    // Test for sum
     @Test
-    @Grade(value=1, cpuTimeout=100)
-    @GradeFeedbacks({@GradeFeedback(message = "", onSuccess = true),
-    @GradeFeedback(message = "sum(int[][] array) does not work\n", onFail = true, onTimeout = true)})
+    @Grade(value=1)
     public void testSum() {
-        
         for(int i = 0; i < 10; i++) {
-            
-            // generate an 2D array that will mostly be an irregular matrix
-            int number_of_row = rng2.get();
-            int [][] expected = new int[number_of_row][];
+            int [][] matrix = generateNonRectangularMatrix();
             int sum = 0;
-
-            for(int row = 0; row < number_of_row; row++) {
-                // random number for column
-                int [] seeds = Stream.generate(rng).limit(rng2.get()).mapToInt(j -> j).toArray();
-                for(int v: seeds) {
-                    sum += v;
-                }
-                expected[row] = seeds;
+            for (int row = 0; row < matrix.length; row ++) {
+                for (int col = 0; col < matrix[row].length; col++)
+                    sum += matrix[row][col];
             }
-
-            // Time to test the student
-            assertEquals(sum, student_code.sum(expected));
-
+            assertEquals(sum, Matrix.sum(matrix));
         }
     }
 
-    // Test for transpose
     @Test
-    @Grade(value=1, cpuTimeout=100)
-    @GradeFeedbacks({@GradeFeedback(message = "", onSuccess = true),
-    @GradeFeedback(message = "int[][] transpose(int[][] matrix) does not work\n", onFail = true, onTimeout = true)})
+    @Grade(value=1)
     public void testTranspose() {
-        
+
         for(int i = 0; i < 10; i++) {
-            
-            // generate an 2D array that will be an regular matrix
-            int number_of_row = rng2.get();
-            int column = rng2.get();
-            int [][] original = generate_regular_matrix(number_of_row, column);
-
-            // compute the real transpose
-            int [][] expected = new int[column][number_of_row];
-            for(int a = 0 ; a < column; a++) {
-                for(int b = 0; b < number_of_row; b++) {
-                    expected[a][b] = original[b][a]; 
-                }
+            int [][] matrix = generateRectangularMatrix();
+            int [][] expected = new int[matrix[0].length][matrix.length];
+            for (int row = 0; row < matrix.length; row ++) {
+                for (int col = 0; col < matrix[0].length; col++)
+                    expected[col][row] = matrix[row][col];
             }
-
-            // Time to test the student
-            assertTrue(
-                Arrays.deepEquals(
-                    expected, 
-                    student_code.transpose(original)
-                )
-            );
-
+            assertArrayEquals(expected, Matrix.transpose(matrix));
         }
     }
 
-    // Test for product
     @Test
-    @Grade(value=1, cpuTimeout=100)
-    @GradeFeedbacks({@GradeFeedback(message = "", onSuccess = true),
-    @GradeFeedback(message = "int[][] product(int[][] matrix1, int[][] matrix2) does not work\n", onFail = true, onTimeout = true)})
+    @Grade(value=1)
     public void testProduct() {
-        
         for(int i = 0; i < 10; i++) {
-            
-            // generate an 2D array that will be an regular matrix
-            int number_of_row = rng2.get();
-            int column = rng2.get();
-            int [][] matrix1 = generate_regular_matrix(number_of_row, column);
-            int column2 = rng2.get();
-            int [][] matrix2 = generate_regular_matrix(column, column2);
+            int [][] m1 = generateRectangularMatrix();
+            int [][] m2 = new int[m1[0].length][rand.nextInt(20)+1];
 
-            // compute the real result
-            int [][] expected = multiplyMatrices(matrix1, matrix2, number_of_row, column, column2);
+            for (int row = 0; row < m2.length; row++) {
+                for (int col=0; col < m2[0].length; col++)
+                    m2[row][col] = rand.nextInt();
+            }
 
-            // Time to test the student
-            assertTrue(
-                Arrays.deepEquals(
-                    expected, 
-                    student_code.product(matrix1, matrix2)
-                )
-            );
+            int [][] expected = new int[m1.length][m2[0].length];
 
+            for (int row = 0; row < m1.length; row ++) {
+                for (int col = 0; col < m2[0].length; col ++) {
+                    int sum = 0;
+                    for (int k = 0; k < m1[0].length; k++)
+                        sum += m1[row][k]*m2[k][col];
+                    expected[row][col] = sum;
+                }
+            }
+            assertArrayEquals(expected, Matrix.product(m1, m2));
         }
     }
 
--- Array2D/unit_test/Tests.java
+++ Array2D/unit_test/Tests.java
@@ -1,168 +1,59 @@
 import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import java.util.function.Supplier;
-import java.util.stream.Stream;
-
-import java.util.*;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
 
 public class Tests {
 
-    // generate random number
-    private Supplier<Integer> rng = () -> (int) ((Math.random()*100) + 1); // never generate 0
-    // random number of row line (just in order to limit the matrix product or other)
-    private Supplier<Integer> rng2 = () -> (int) ((Math.random()*10) + 1); // never generate 0
-    // Student code
-    private Array2DBuilderInterface student_code = new MyBuilder();
-
-    // build a regular matrix
-    private int[][] generate_regular_matrix(int number_of_row, int number_of_column) {
-        int [][] original = new int[number_of_row][];
-
-        for(int row = 0; row < number_of_row; row++) {
-            int [] seeds = Stream.generate(rng).limit(number_of_column).mapToInt(i -> i).toArray();
-            original[row] = seeds;
-        }
-
-        return original;
-    }
-
-    // compute matrix multiplication
-    // Credits to https://www.programiz.com/java-programming/examples/multiply-matrix-function
-    // (code on stackoveflow was ugly as hell)
-    private int[][] multiplyMatrices(int[][] firstMatrix, int[][] secondMatrix, int r1, int c1, int c2) {
-        int[][] product = new int[r1][c2];
-        for(int i = 0; i < r1; i++) {
-            for (int j = 0; j < c2; j++) {
-                for (int k = 0; k < c1; k++) {
-                    product[i][j] += firstMatrix[i][k] * secondMatrix[k][j];
-                }
-            }
-        }
-        return product;
-    }
-
-    // Test for build_from
     @Test
     public void testBuildFrom() {
-
-        for(int i = 0; i < 10; i++) {
-
-            // generate an 2D array that will mostly be an irregular matrix
-            int number_of_row = rng2.get();
-            int [][] expected = new int[number_of_row][];
-            StringBuilder str = new StringBuilder();
-
-            for(int row = 0; row < number_of_row; row++) {
-
-                int [] seeds = Stream.generate(rng).limit(rng2.get()).mapToInt(j -> j).toArray();
-                for(int v: seeds) {
-                    str.append(String.valueOf(v) + " ");
-                }
-                expected[row] = seeds;
-                str.append("\n");
-                if (row != number_of_row -1) {
-                    str.append(" ");
-                }
-
-            }
-
-            // Time to stringify this thing for student
-            String input = str.toString();
-            assertTrue(
-                    Arrays.deepEquals(
-                            expected,
-                            student_code.build_from(input)
-                    )
-            );
-
-        }
+        int [][] matrix = new int[]{
+            new int[]{1, 2, 3},
+            new int[]{10, 11}
+        };
+        String repr = "1 2 3\n 10 11";
+        assertEquals(matrix, Matrix.buildFrom(repr));
     }
 
-    // Test for sum
     @Test
     public void testSum() {
-
-        for(int i = 0; i < 10; i++) {
-
-            // generate an 2D array that will mostly be an irregular matrix
-            int number_of_row = rng2.get();
-            int [][] expected = new int[number_of_row][];
-            int sum = 0;
-
-            for(int row = 0; row < number_of_row; row++) {
-                // random number for column
-                int [] seeds = Stream.generate(rng).limit(rng2.get()).mapToInt(j -> j).toArray();
-                for(int v: seeds) {
-                    sum += v;
-                }
-                expected[row] = seeds;
-            }
-
-            // Time to test the student
-            assertEquals(sum, student_code.sum(expected));
-
-        }
+        int [][] matrix = new int[]{
+            new int[]{1, 2, 3},
+            new int[]{10, 11}
+        };
+        assertEquals(27, Matrix.sum(matrix));
     }
 
-    // Test for transpose
     @Test
     public void testTranspose() {
-
-        for(int i = 0; i < 10; i++) {
-
-            // generate an 2D array that will be an regular matrix
-            int number_of_row = rng2.get();
-            int column = rng2.get();
-            int [][] original = generate_regular_matrix(number_of_row, column);
-
-            // compute the real transpose
-            int [][] expected = new int[column][number_of_row];
-            for(int a = 0 ; a < column; a++) {
-                for(int b = 0; b < number_of_row; b++) {
-                    expected[a][b] = original[b][a];
-                }
-            }
-
-            // Time to test the student
-            assertTrue(
-                    Arrays.deepEquals(
-                            expected,
-                            student_code.transpose(original)
-                    )
-            );
-
-        }
+        int [][] matrix = new int[]{
+            new int[]{1, 2, 3},
+            new int[]{10, 11}
+        };
+        int [][] transposed = new int[]{
+            new int[]{1, 10},
+            new int[]{2, 11},
+            new int[]{3}
+        };
+        assertArrayEquals(transposed, Matrix.transpose(matrix));
     }
 
-    // Test for product
     @Test
     public void testProduct() {
+        int [][] matrix = new int[]{
+            new int[]{1, 2, 3},
+            new int[]{10, 11, 12}
+        };
+        int [][] mult = new int[]{
+            new int[]{4, 13},
+            new int[]{5, 14},
+            new int[]{6, 15}
+        };
 
-        for(int i = 0; i < 10; i++) {
-
-            // generate an 2D array that will be an regular matrix
-            int number_of_row = rng2.get();
-            int column = rng2.get();
-            int [][] matrix1 = generate_regular_matrix(number_of_row, column);
-            int column2 = rng2.get();
-            int [][] matrix2 = generate_regular_matrix(column, column2);
-
-            // compute the real result
-            int [][] expected = multiplyMatrices(matrix1, matrix2, number_of_row, column, column2);
-
-            // Time to test the student
-            assertTrue(
-                    Arrays.deepEquals(
-                            expected,
-                            student_code.product(matrix1, matrix2)
-                    )
-            );
-
-        }
+        int [][] product = new int[]{
+            new int[]{28, 86},
+            new int[]{167, 284}
+        };
+        assertArrayEquals(product, Matrix.product(matrix));
     }
-
 }
