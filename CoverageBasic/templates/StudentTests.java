bdd00ae3150287fd4b7fd1f9c320159cdf4a350b
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2020-11-05 11:26:56 +0100

    add basic and intermediate coverage exercises

---
+++ CoverageBasic/templates/StudentTests.java
@@ -0,0 +1,18 @@
+package src;
+
+import com.github.guillaumederval.javagrading.GradeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import flavour.*;
+
+import static org.junit.Assert.*;
+
+@RunWith(JailRunner.class)
+@GradeClass(totalValue = 1, defaultCpuTimeout = 2000)
+public class StudentTests {
+
+    // STUDENT CODE WILL BE INSERTED HERE
+@ @student_code@@
+    // END OF STUDENT CODE
+
+}
